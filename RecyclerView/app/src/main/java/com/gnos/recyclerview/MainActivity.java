3371d566b538052a5cd47113b20a7bdf930b7f40
bhsong <manin21st@gmail.com>
2019-08-03 00:01:14 +0900

    2019.08.23 - RecyclerView 기본 세팅

---
+++ RecyclerView/app/src/main/java/com/gnos/recyclerview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.gnos.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private String[] myDataset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        myDataset = new String[100];  // 배열초기화
+        for (int i=0;i<100;i++) {
+            myDataset[i] = "RecyclerView Row-" + i;
+        }
+        // specify an adapter (see also next example)
+        adapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ RecyclerView/app/src/main/java/com/gnos/recyclerview/MyAdapter.java
@@ -0,0 +1,60 @@
+package com.gnos.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        // each data item is just a string in this case
+        public TextView textView;
+        public MyViewHolder(View v) {
+            super(v);
+            textView = v.findViewById(R.id.tv_text);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    @NonNull
+    @Override
+    // Create new views (invoked by the layout manager)
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        // create a new view
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.text_adapter, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    // Replace the contents of a view (invoked by the layout manager)
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(String.valueOf(mDataset[position]));
+    }
+
+    @Override
+    // Return the size of your dataset (invoked by the layout manager)
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+}
